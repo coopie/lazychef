e328082abcb6beeac9c078ed5b0db4136435abb7
Sam Coope <sam.j.coope@gmail.com>
2017-01-31 00:27:07 +0000

    Add refactoring for a more sane generators.

--- test/generators_test.py
+++ test/generators_test.py
@@ -4,46 +4,41 @@ import numpy as np
 import h5py
 import os
 
-from lazychef.keras_generators import KerasGenerator, LabeledKerasGenerator
+from lazychef.generators import Generator, DatasetGenerator, ShuffleDatasetCallback
+
+TEST_FILE = 'generators.data.hdf5'
 
 
-TEST_FILE = 'keras_generators.data.hdf5'
-
-
-class TestKerasGenerators(unittest.TestCase):
+class TestGenerators(unittest.TestCase):
 
     def test_dataset_exists(self):
         with h5py.File(TEST_FILE, 'r') as f:
             self.assertTrue('data' in f)
 
-
     def test_fitting_batch_size(self):
         f = h5py.File(TEST_FILE, 'r')
-        gen = KerasGenerator(f['data'], batch_size=1)
+        gen = Generator([f['data']], batch_size=1)
         a = []
-        for i in range(4):
-            a.append(gen.__next__())
+        for i in range(1, 4):
+            a.append(gen.__next__()[0])
 
         self.assertEqual(
             len(a),
-            4
+            3
         )
 
         expected_result = [(np.repeat((i % 3) + 1, 3), np.repeat((i % 3) + 1, 3)) for i in range(3)]
         for actual, expected in zip(a, expected_result):
             self.assertTrue(
-                np.all(expected[1] == actual[0])
-            )
-            self.assertTrue(
-                np.all(expected[1] == actual[1])
+                np.all(expected == actual)
             )
 
     def test_not_well_fitting_batch_size(self):
         f = h5py.File(TEST_FILE, 'r')
-        gen = KerasGenerator(f['data'], batch_size=2)
+        gen = DatasetGenerator([f['data']], batch_size=2)
         a = []
         for i in range(2):
-            a.append(gen.__next__())
+            a.append(next(gen)[0])
 
         self.assertEqual(
             len(a),
@@ -51,23 +46,18 @@ class TestKerasGenerators(unittest.TestCase):
         )
 
         x = np.array([np.repeat(1, 3), np.repeat(2, 3)])
-        one_next_call = (x, x)
-        expected_result = [one_next_call, one_next_call]
+        expected_result = [x, x]
 
         for actual, expected in zip(a, expected_result):
-            for i in range(2):
-                for j in range(2):
-                    self.assertTrue(
-                        np.all(expected[i][j] == actual[i][j])
-                    )
-                    self.assertTrue(
-                        np.all(expected[i][j] == actual[i][j])
-                    )
+            self.assertTrue(
+                np.array_equal(
+                    expected, actual
+                )
+            )
 
-
-    def labeled_keras_generator_test(self):
+    def test_labeled_generator(self):
         f = h5py.File(TEST_FILE, 'r')
-        gen = LabeledKerasGenerator(f['data'], f['labels'], batch_size=3)
+        gen = DatasetGenerator([f['data'], f['labels']], batch_size=3)
 
         X, Y = next(gen)
         np.testing.assert_equal(
@@ -79,7 +69,25 @@ class TestKerasGenerators(unittest.TestCase):
             np.array([np.repeat(x, 3) for x in range(1, 4)])
         )
 
+    def test_shuffling(self):
+        f = h5py.File(TEST_FILE, 'r')
 
+        shuffle_callback = ShuffleDatasetCallback(seed=1237)
+        gen = DatasetGenerator([f['data']], batch_size=1, callbacks=[shuffle_callback])
+        a = []
+        for i in range(6):
+            a.append(next(gen)[0])
+
+        self.assertEqual(
+            len(a),
+            6
+        )
+        self.assertFalse(
+            np.array_equal(
+                np.array(a[:3]),
+                np.array(a[3:])
+            )
+        )
 
     @classmethod
     def setUpClass(cls):
@@ -90,12 +98,11 @@ class TestKerasGenerators(unittest.TestCase):
         labels = np.array([[x] for x in range(1, 4)])
         f['labels'] = labels
 
-
-
     @classmethod
     def tearDownClass(cls):
         if os.path.exists(TEST_FILE):
             os.remove(TEST_FILE)
+
 
 if __name__ == '__main__':
     unittest.main()
